c45ce479c593323e116c1d304655d8d4f4d73156
dianaeftaiha <dianaeftaiha@gmail.com>
2021-07-07 11:13:01 +0300

    send async requests

---
+++ async.php
@@ -0,0 +1,24 @@
+<?php
+
+require 'vendor/autoload.php';
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Exception\RequestException;
+
+$client = new Client();
+
+$promise=$client->requestAsync(
+    'GET',
+    'https://jsonplaceholder.typicode.com/posts/1'
+);
+
+$promise->then(
+    function (Response $resp){
+        echo $resp->getBody();
+    },
+    function (RequestException $e){
+        echo $e->getMessage();
+    }
+);
+
+$promise->wait();
